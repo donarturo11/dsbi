ce658f5a487d7bb231021c0518b194c630d91e91
Artur Wrona <arturwrona91@gmail.com>
2024-01-03 21:15:39 +0100

    Rewrite sampling, move soft TX to interrupt, prepare queue

--- firmware/attiny13-dsbi-transmitter/main.c
+++ firmware/attiny13-dsbi-transmitter/main.c
@@ -1,5 +1,6 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <avr/cpufunc.h>
 #include <stdio.h>
 #include <util/delay.h>
 #include <compat/deprecated.h>
@@ -8,25 +9,76 @@
 #define BITLENGTH (1000000/BAUDRATE) - 1
 #define TXPIN PB1
 #define TESTPIN PB4
-
+#define BUFFER_SIZE 12
 static uint16_t sampledValue = 0;
 static uint8_t sample_cnt = 0;
+struct _queue_t;
+typedef struct _queue_t {
+    uint8_t data;
+    struct *_queue_t next;
+} queue_t;
 
+static queue_t TXQUEUE;
+
+
+// =======================================
+struct {
+    uint16_t frame;
+    uint8_t busy;
+} TXBUF;
+
+void TXBUF_sendbyte(uint8_t byte) {
+    if (TXBUF.busy) return;
+    cli();
+    TXBUF.frame = 1 << 9;
+    TXBUF.frame |= ((uint16_t) byte << 1) ;
+    TXBUF.busy = 10;
+    OCR0B = 240;
+    TCNT0 = 0;
+    sei();
+}
+
+void TIMER_init() {
+    TCCR0B = 0;
+    TCCR0B |= 0b001;
+    sbi(TIMSK0, OCIE0B);
+    sbi(TIFR0, OCF0B);
+    
+}
+
+ISR(TIM0_COMPB_vect) {
+    cli();
+    //PORTB ^= 1 << TESTPIN;
+    if (TXBUF.busy) {
+        sbi(PORTB, TESTPIN);
+        if (TXBUF.frame & 1) sbi(PORTB, TXPIN);
+        else cbi(PORTB, TXPIN);
+        TXBUF.busy--;
+        TXBUF.frame >>= 1;
+    } else {
+        cbi(PORTB, TESTPIN);
+    }
+    TCNT0 = 0;
+    sei();
+}
+
+// =====
 void TX_setup();
 void TX_putc(uint8_t value);
 
+
+/*
 ISR(ADC_vect) {
-    if(sample_cnt == 8) sbi(PORTB, TESTPIN);
+    //if(sample_cnt == 8) sbi(PORTB, TESTPIN);
     if(sample_cnt == 16) {
         cli();
-        sbi(ADCSRA, ADSC);
-        TX_putc(0xAA);
-        cbi(PORTB, TESTPIN);
+        //cbi(PORTB, TESTPIN);
         short adc = 0;
         adc = sampledValue / 16;
+        BUFFER_write(0xAA);
+        BUFFER_write((adc >> 8) & 0xFF);
+        BUFFER_write(adc & 0xFF);
         sei();
-        TX_putc((adc >> 8) & 0x0F);
-        TX_putc(adc & 0xFF);
         sample_cnt = 0;
         sampledValue = 0;
     }
@@ -34,7 +86,7 @@ ISR(ADC_vect) {
     sample_cnt++;
     sbi(ADCSRA, ADSC);
 }
-
+*/
 void testpin_init() {
     cbi(PORTB, TESTPIN);
 }
@@ -47,25 +99,17 @@ void adc_init() {
     cbi(ADCSRA, ADPS1);
     sbi(ADCSRA, ADPS0);
     sbi(ADCSRA, ADEN);
-    sbi(ADCSRA, ADIE);
-    sbi(ADCSRA, ADIF);
+    //sbi(ADCSRA, ADIE);
+    //sbi(ADCSRA, ADIF);
 }
 
 int adc_read() {
     sbi(PORTB, TESTPIN);
     ADCSRA |= (1 << ADSC);
     while (ADCSRA & (1 << ADSC));
-    //cbi(PORTB, TESTPIN);
-    return ((ADCH << 2) | ADCL);
+    return ((ADCH << 8) | ADCL);
 }
-
-
-
-void TX_setup() {
-    sbi(DDRB, TXPIN); // setup pin as output
-    sbi(PORTB, TXPIN); // setup TTL as idle
-}
-
+/*
 void TX_putc(uint8_t value) {
     uint8_t counter = 1;
     cbi(PORTB, TXPIN); // put start bit
@@ -83,16 +127,37 @@ void TX_putc(uint8_t value) {
     _delay_us(BITLENGTH);
     _delay_us(BITLENGTH);
 }
+*/
+void TX_setup() {
+    sbi(DDRB, TXPIN); // setup pin as output
+    sbi(PORTB, TXPIN); // setup TTL as idle
+}
 
 int main() {
     cli();
+    //BUFFER_init();
     testpin_init();
     TX_setup();
     adc_init();
+    TIMER_init();
     sei();
-    sbi(ADCSRA, ADSC);
+    //sbi(ADCSRA, ADSC);
+    uint8_t data = 0;
+    cbi(PORTB, TESTPIN);
     while(1) {
-
+        sampledValue = adc_read();
+        if (sample_cnt == 16) {
+            short adc = 0;
+            adc = sampledValue / 16;
+            //BUFFER_write(0xAA);
+            //BUFFER_write((adc >> 7) & 0xFF);
+            //BUFFER_write(adc & 0xFF);
+            _delay_us(20);
+            sample_cnt = 0;
+        } else {
+            _delay_us(100);
+            sample_cnt++;
+        }
     }
     return 0;
 }
